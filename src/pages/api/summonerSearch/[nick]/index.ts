3528e0580a40c9742e52116750d6aa363560185c
Thiago Trres <thiagomev@gmail.com>
2022-04-06 15:45:05 -0300

    Add get summoner rank status

--- src/pages/api/summonerSearch/[nick]/index.ts
+++ src/pages/api/summonerSearch/[nick]/index.ts
@@ -1,11 +1,30 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { riotGamesApi } from '../../../../services/api';
 
+interface ISummnoerData {
+  id: string;
+  accountId: string;
+  puuid: string;
+  name: string;
+  profileIconId: number;
+  revisionDate: number;
+  summonerLevel: number;
+}
+
 export default async function search(req: NextApiRequest, res: NextApiResponse) {
   const { nick } = req.query;
   
-  const summonerData = await riotGamesApi.get(
+  const summonerData = await riotGamesApi.get<ISummnoerData>(
     "summoner/v4/summoners/by-name/" + nick);
 
-  return res.status(200).json({ data: summonerData.data })
+  const summonerRank = await riotGamesApi.get(
+      "league/v4/entries/by-summoner/" + summonerData.data.id);
+  
+
+  return res.status(200).json({ 
+     data: {
+       summoner: summonerData.data,
+       summonerRank: summonerRank.data
+     }
+  });
 }

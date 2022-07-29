893ede381338a59dbf64d5ecbf5a25211854faaa
Thiago Torres <thiagomev@gmail.com>
2022-07-28 22:59:29 -0300

    Refactory api ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
 
+const tokenApi:any = process.env.RIOT_API_KEY
+
 export const riotGamesApiBr1 = axios.create({
   baseURL: "https://br1.api.riotgames.com/lol/",
   headers: {
-    "X-Riot-Token": process.env.RIOT_API_KEY,
+    "X-Riot-Token": tokenApi,
   },
 });
 
 export const riotGamesApiAmericas = axios.create({
   baseURL: "https://americas.api.riotgames.com/lol/",
   headers: {
-    "X-Riot-Token": process.env.RIOT_API_KEY,
+    "X-Riot-Token":tokenApi,
   },
 });
